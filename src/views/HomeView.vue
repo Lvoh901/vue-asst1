fd65667481abf3d22e5b57f2cb39221a9b467435
Elvis Odhiambo <ochiengelvis90@gmail.com>
2024-03-12 19:18:22 +0300

    functional setup and structure links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,19 @@
 <template>
   <div class="home">
     <h1>Welcome</h1>
+
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias ab quis quae. Quam laborum
+      modi natus, incidunt a dolore molestias, ducimus consectetur odio asperiores rerum.
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est dicta officiis debitis quas
+      quidem, repellendus reiciendis architecto asperiores quis consequuntur minima tempora.
+      Excepturi nemo suscipit ipsa dolorem nobis deleniti numquam! Repudiandae exercitationem
+      adipisci minus omnis, voluptates quibusdam dolorem quis quia ad, voluptate laborum aut? Natus
+      quasi dicta odit libero expedita?
+    </p>
   </div>
 </template>
 
@@ -14,5 +27,11 @@
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: 5em;
+}
+
+p {
+  text-align: center;
+  font-size: 1.5em;
 }
 </style>
